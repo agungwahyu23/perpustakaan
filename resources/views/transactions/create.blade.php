856e2eca618d2ad6bca966499240585ace4a53cf
agungwahyu23 <52806335+agungwahyu23@users.noreply.github.com>
2025-07-17 11:49:55 +0700

    update select2

--- resources/views/transactions/create.blade.php
+++ resources/views/transactions/create.blade.php
@@ -1,5 +1,26 @@
 @extends('app')
 
+@push('stylesheets')
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+    <style>
+        .select2-container--default .select2-selection--single {
+            height: 40px;
+            padding: 6px 12px;
+            border: 1px solid #ced4da;
+            border-radius: 0.375rem;
+        }
+
+        .select2-container--default .select2-selection--single .select2-selection__rendered {
+            line-height: 24px;
+        }
+
+        .select2-container--default .select2-selection--single .select2-selection__arrow {
+            height: 36px;
+            right: 10px;
+        }
+    </style>
+@endpush
+
 @section('content')
 <form action="{{ route('transactions.store') }}" method="POST" enctype="multipart/form-data">
     @csrf
@@ -7,14 +28,14 @@
         <div class="col-md-4">
             <div class="form-group">
                 <label for="kode" class="text-white">Kode Transaksi:</label>
-                <input type="text" value="{{ $kode }}" class="form-control @error('kode') is-invalid @enderror" id="kode" name="kode" placeholder="Masukkan Kode" readonly>
+                <input type="text" value="{{ $kode }}" class="form-select @error('kode') is-invalid @enderror" id="kode" name="kode" placeholder="Masukkan Kode" readonly>
                 @error('kode') <div class="invalid-feedback">{{ $message }}</div> @enderror
             </div>
         </div>
         <div class="col-md-4">
             <div class="form-group">
                 <label for="user_id" class="text-white">Nama Peminjam:*</label>
-                <select name="user_id" id="user_id" class="form-control form-select" required>
+                <select name="user_id" id="user_id" class="form-select form-select js-example-basic-single" required>
                     <option value="">Pilih Nama Peminjam</option>
                     @foreach ($user as $item)
                         <option value="{{ $item->id }}">{{ $item->name }}</option>
@@ -26,7 +47,7 @@
         <div class="col-md-4">
             <div class="form-group">
                 <label for="book_id" class="text-white">Judul Buku:*</label>
-                <select name="book_id" id="book_id" class="form-control form-select" required>
+                <select name="book_id" id="book_id" class="form-control form-select js-example-basic-single" required>
                     <option value="">Pilih Buku</option>
                     @foreach ($book as $item)
                         <option value="{{ $item->id }}">{{ $item->judul }}</option>
@@ -40,3 +61,13 @@
     <a href="{{ route('users.index') }}" class="btn btn-warning">Kembali</a>
 </form>
 @endsection
+
+@push('scripts')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('.js-example-basic-single').select2();
+        });
+    </script>
+@endpush
